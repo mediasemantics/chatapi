e8838d6c676e0eb3ea92f9bf4664c3d52e87286f
Doug Copeland <doug>
2020-05-08 12:59:37 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
-# chatapi
-Media Semantics Chatbot API Reference Implementation
+# Media Semantics Chatbot API Reference Implementation
+Server implementation of Chatbot API
+
+## Overview
+This is the Reference Implementation for the [Media Semantics Chatbot API](https://aws.amazon.com/marketplace/pp/B06ZY1VBFZ), a cloud-based API available on the Amazon AWS Marketplace.
+
+For a detailed introduction to the Chatbot API, please read the [Chatbot API Tutorial](https://www.mediasemantics.com/apitutorial4.html). 
+
+You can see the Reference Implementation running [here](https://mediasemantics.com/chatapiclient.html). 
+
+## Requirements
+This README assumes that you are able to view html pages using a local web server (i.e. using a url that begins with http://localhost) and that you are able to run Node.js. 
+If you prefer, you can also install it directly on a web server. Please see this [tutorial](https://www.mediasemantics.com/apitutorial2.html) for tips on
+setting up an AWS EC2 instance using Apache and Node.js.
+
+## Obtaining keys
+Use this [AWS Markeplace](https://aws.amazon.com/marketplace/pp/B06ZY1VBFZ) page to add the
+Chatbot service to your AWS account. You will receive codes by email that you will insert in the server.js file. You will be charged $0.001 per call to the 'reply' endpoint. There are no monthly minimums. 
+Charges will appear on your monthly AWS bill. The access key is the 8-digit key that is mailed to you when you add the Chatbot API to your AWS account. 
+
+## Installation
+Install the sample, e.g. in the home directory:
+```
+cd ~  
+git clone https://github.com/mediasemantics/chatapi.git  
+cd chatapi
+```
+
+Install the required dependencies:
+```
+npm update
+```
+
+Create a conversations subdirectory:
+```
+mkdir conversations
+```
+
+Modify the server.js file to add your Chatbot API access credentials.
+```
+nano server.js
+```
+Replace 'xxxxxxxx' with the 8 digit key that was mailed to you when you signed up for Chatbot API.
+
+Save your changes.
+
+You can now start the server with:
+```
+node server.js
+```
+You should see "Listening on port 3000".
+
+You can verify that the caching server app is working by viewing this URL from a web browser:
+```
+http://localhost:3000/reply&userid=123&input=hello
+```
+You should see an answer.
+
+Finally, load chatapiclient.html into a web browser using the appropriate http://localhost url (the sample will not run using a file url).
+
+## Going Further
+
+Please see the [Chatbot API Tutorial](https://www.mediasemantics.com/apitutorial4.html) for a more detailed description of the Chatbot API and the Chatbot API Reference Implementation.
+
+
+
+
---
+++ server.js
@@ -0,0 +1,108 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var request = require('request');
+
+// TODO set up your Chartbot API key here
+var chatAPIKey = "xxxxxxxx";
+
+// TODO - this is a sample rulebase - change to your own rulebase
+var rulebaseURL = "http://s3-us-west-2.amazonaws.com/samples.mediasemantics.com/sample-5-6-20.json";
+
+// TODO set the path to your cache directory, and make sure to give it read/write permission, e.g. mkdir conversations && sudo chgrp apache conversations && sudo chmod g+w conversations
+var cachePrefix = "./conversations/";
+
+// Set up express
+var app = express();
+app.use(bodyParser.json({limit: '1mb'}));
+app.use(bodyParser.urlencoded({ limit: '1mb', extended: true }));
+
+// The Chatbot API endpoint
+var urlReply = "http://characterapi.com/reply";
+
+
+app.get('/reply', function(req, res, next) {
+    if (!req.query.userid) throw new Error("missing userid");
+    if (!req.query.input) throw new Error("missing input");
+    
+    // Load the data for this user from disk, if available
+    let filename = cachePrefix + req.query.userid + '.json';
+    fs.readFile(filename, "utf8", function(err, raw) {
+        let data;
+        let control;
+        
+        // No data available - treat as a new user
+        if (err) {  
+            data = {};
+            control = "[start]";
+        }
+        // Conversation data is available for this user
+        else {  
+            data = JSON.parse(raw);
+            // Also read last-modify time
+            let stats = fs.statSync(filename);
+            let lastModified = new Date(stats.mtime);
+            // Calculate minutes since last input/output
+            let elapsedMinutes = Math.floor((new Date() - lastModified) / 60000);
+            // Add some context when there are long breaks in the conversation
+            if (elapsedMinutes > 5)
+                control = "[restart " + elapsedMinutes + "]";
+        }
+        
+        // If we have synthesized a [start] or [restart n] event, send that first, ignore the output.
+        sendControlIfNecessary(control, data, req, res, next, function(data) {
+            
+            // Now send the user input (or [auto])
+            let params = {
+                key:chatAPIKey,
+                input:req.query.input,
+                data:JSON.stringify(data),
+                url:rulebaseURL
+            }
+            request.post({url:urlReply, form:params}, function (err, httpResponse, body) {            
+                if (err) return next(err);
+                else if (httpResponse.statusCode != 200) return next(new Error("chat error"))
+                let ret = JSON.parse(body);
+                
+                // Write the data back again
+                fs.writeFile(filename, JSON.stringify(ret.data), function(err) {
+                    if (err) return next(err);
+                    
+                    // Return the response
+                    res.statusCode = 200;
+                    if ((req.get("Origin")||"").indexOf("localhost") != -1) res.setHeader('Access-Control-Allow-Origin', req.get("Origin"));
+                    // TODO: adjust your domain for CORS protection
+                    // if ((req.get("Origin")||"").indexOf("yourdomain") != -1) res.setHeader('Access-Control-Allow-Origin', req.get("Origin"));
+                    res.setHeader('content-type', 'application/json');
+                    res.write(JSON.stringify({output:ret.output, idle:ret.idle}));
+                    res.end();
+                    if (data.idle)
+                        setIdleTimer(data.idle);
+                });
+            });
+        });
+    });
+});
+
+
+// If control is defined, then sends a control input, then calls the callback with the new data. 
+// Otherwise, calls the callback with the existing data.
+function sendControlIfNecessary(control, data, req, res, next, callback) {
+    if (!control) return callback(data);
+    let params = {
+        key:chatAPIKey,
+        input:control,
+        data:JSON.stringify(data),
+        url:rulebaseURL
+    }
+    request.post({url:urlReply, form:params}, function (err, httpResponse, body) {            
+        if (err) return next(err);
+        else if (httpResponse.statusCode != 200) return next(new Error("chat error"))
+        let ret = JSON.parse(body);
+        callback(ret.data);
+    });
+}
+        
+app.listen(3000, function() {
+  console.log('Listening on port 3000');
+});
